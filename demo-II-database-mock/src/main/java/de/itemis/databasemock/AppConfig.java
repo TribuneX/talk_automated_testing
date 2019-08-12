44ef676506ce97149f7d7ec28f1e52255866edee
TribuneX <TribuneX@users.noreply.github.com>
2019-08-12 15:24:26 +0200

    step 5: remove spring dependency from contact service

---
+++ demo-II-database-mock/src/main/java/de/itemis/databasemock/AppConfig.java
@@ -0,0 +1,13 @@
+package de.itemis.databasemock;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ContactService createContactService(final ContactRepository repository) {
+        return new ContactService(repository);
+    }
+}
--- demo-II-database-mock/src/main/java/de/itemis/databasemock/ContactService.java
+++ demo-II-database-mock/src/main/java/de/itemis/databasemock/ContactService.java
@@ -1,12 +1,7 @@
 package de.itemis.databasemock;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
 public class ContactService {
 
-    @Autowired
     private ContactRepository repository;
 
     public ContactService(ContactRepository repository) {
