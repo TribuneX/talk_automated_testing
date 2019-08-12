b4e1520ce4fc618ffe2834dd4cc71ee99e784a8b
TribuneX <TribuneX@users.noreply.github.com>
2019-08-12 15:53:18 +0200

    upgrade selenium web driver

--- demo-I-register-app/src/test/java/de/itemis/registerpage/AbstractUiTest.java
+++ demo-I-register-app/src/test/java/de/itemis/registerpage/AbstractUiTest.java
@@ -1,6 +1,6 @@
 package de.itemis.registerpage;
 
-import io.github.bonigarcia.SeleniumExtension;
+import io.github.bonigarcia.seljup.SeleniumExtension;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
